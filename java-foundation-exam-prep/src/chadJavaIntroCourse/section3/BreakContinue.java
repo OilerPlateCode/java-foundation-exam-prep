5b22800eb2a4cfb4f04d98c3bcb7acd80ef169a6
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-07-25 22:37:41 +0200

    chapter 3 - leanring break

---
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section3/BreakContinue.java
@@ -0,0 +1,12 @@
+package chadJavaIntroCourse.section3;
+
+public class BreakContinue {
+    public static void main(String[] args) {
+        for(int x = 0; x < 10; x++) {
+            if(x == 5) {
+                break;
+            }
+            System.out.println(x);
+        }
+    }
+}

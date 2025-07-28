21bed8696b0ca9ee88cacddba61ca7235dc793c2
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-07-28 10:47:46 +0200

    chapter 3 - leanring continue

--- java-foundation-exam-prep/src/chadJavaIntroCourse/section3/BreakContinue.java
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section3/BreakContinue.java
@@ -8,5 +8,33 @@ public class BreakContinue {
             }
             System.out.println(x);
         }
+        System.out.println("What we observed from the break os it stops when the condition is met");
+        System.out.println("This is the continue");
+
+        for(int x = 0; x < 10; x++) {
+
+            if (x == 4) {
+                continue;
+            }
+            System.out.println(x);
+        }
+
+        System.out.println("What we observed from the continue is that it skips the number it is at");
+        System.out.println("Now we use greater and less than");
+
+        for(int x = 0; x < 10; x++) {
+            if (x < 3) {
+                continue;
+            }
+            System.out.println(x);
+        }
+        System.out.println("BREAK");
+        for(int x = 0; x < 10; x++) {
+            if (x > 6) {
+                break;
+            }
+
+            System.out.println(x);
+        }
     }
 }

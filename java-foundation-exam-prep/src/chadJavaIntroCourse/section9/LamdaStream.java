8aa70efa8b984e29ec8e760c1d6e150786aaadc1
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-09-26 21:39:56 +0200

    practice - rehersing enhanced for loop and classic for loop

--- java-foundation-exam-prep/src/chadJavaIntroCourse/section9/LamdaStream.java
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section9/LamdaStream.java
@@ -11,6 +11,15 @@ public class LamdaStream {
         countriesToVisit.add("Zimbabwe");
         countriesToVisit.add("Namibia");
 
+        for (String country : countriesToVisit) {
+            System.out.println("I am yet to visit " + country);
+        }
+
+        System.out.println(" classic ==== for loop ==");
+        for (int i = 0 ; i < countriesToVisit.size(); i++) {
+            System.out.println("I should plan a visit to " + countriesToVisit.get(i));
+        }
+        System.out.println(" Lambda ");
         countriesToVisit.forEach((country) -> {
             System.out.println("I want to visit " + country);
         });

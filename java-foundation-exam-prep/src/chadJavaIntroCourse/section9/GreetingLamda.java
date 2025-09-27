94092ef918c1d7797bf2a473f33c4828c674900a
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-09-27 20:21:00 +0200

    learn - creating own lamda

--- java-foundation-exam-prep/src/chadJavaIntroCourse/section9/GreetingLamda.java
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section9/GreetingLamda.java
@@ -4,8 +4,15 @@ public class GreetingLamda {
     public static void main(String[] args) {
         GreetingImpl greetingImpl = new GreetingImpl();
         greetingImpl.greeting();
+
+        Greeting lambda = () -> System.out.println("Hello Lambda");
+        lambda.greetings();
     }
 
+}
+
+interface Greeting {
+    void greetings();
 }
 
  class GreetingImpl {

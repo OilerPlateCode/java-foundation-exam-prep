c4c60779d656811f042aaba3e6ccb0c6ddf24405
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-09-29 19:57:56 +0200

    practice - looking at lambda with method reference

--- java-foundation-exam-prep/src/excercises/LambdaRevision.java
+++ java-foundation-exam-prep/src/excercises/LambdaRevision.java
@@ -4,9 +4,16 @@ public class LambdaRevision {
     public static void main(String[] args) {
         PassportStamp passportStamp = (d) -> System.out.println(d);
         passportStamp.trip("Heading to Namibia Next");
+
+        PreviousTrip prviousTrip = System.out::println;
+        prviousTrip.trip("Went to Botswana Last");
     }
 }
 
 interface PassportStamp {
+    void trip(String destination);
+}
+
+interface PreviousTrip {
     void trip(String destination);
 }

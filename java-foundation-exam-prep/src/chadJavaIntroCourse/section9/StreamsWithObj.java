58de9edbf1709bcb6e19bbc40ba0131c87c35ce1
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-10-02 10:54:00 +0200

    learn - learning streams with objects

---
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section9/StreamsWithObj.java
@@ -0,0 +1,27 @@
+package chadJavaIntroCourse.section9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamsWithObj {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, "Simangele", 1));
+        employees.add(new Employee(2, "Samukelo", 2));
+        employees.add(new Employee(3, "Sipho", 3));
+        employees.add(new Employee(4, "Bongani", 4));
+        employees.add(new Employee(5, "Kabelo", 5));
+        employees.add(new Employee(6, "Naledi", 6));
+        employees.add(new Employee(7, "Nkhensani", 7));
+
+        int employeeYearsAbove4 = 0;
+
+        for (Employee employee : employees) {
+            if (employee.getYearsOfService() > 4) {
+                employeeYearsAbove4++;
+            }
+        }
+        System.out.println(employeeYearsAbove4);
+    }
+}
+

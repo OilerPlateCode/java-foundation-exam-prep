6797af9736ea973959176b6e7e191f8da4ad8612
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-10-09 18:21:19 +0200

    learn - experimenting with the try catch

--- java-foundation-exam-prep/src/chadJavaIntroCourse/section8/ExceptionHandlingWithNumbers.java
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section8/ExceptionHandlingWithNumbers.java
@@ -10,28 +10,34 @@ public class ExceptionHandlingWithNumbers {
         int numberInt = Integer.parseInt(numberString);
 
         System.out.println(numberInt);
-
+        parseString(null);
+        parseString("one");
+        parseString("2");
         try {
             int notNumberInt = Integer.parseInt(notNumberString);
             System.out.println(notNumberInt);
+
         } catch (IllegalArgumentException e) {
             System.out.println("Should pass in a String that is inty");
             System.out.println(e);
             System.out.println(e.getMessage());
 //            throw new RuntimeException(e);
+
         }
     }
 
-    public void parseString(String numberString) {
+    public static void parseString(String numberString) {
         try {
+            System.out.println(numberString.length());
             int numberInt = Integer.parseInt(numberString);
+            System.out.println(numberInt);
         }
         catch (NumberFormatException | NullPointerException e) {
-            System.out.println("Should pass in a String that is inty");
+            System.out.println("Should pass in a String that is an int");
         }
 
         catch (Exception e) {
-            System.out.println("Something went wrong");
+            System.out.println("Eyyyyyy ==== Something went wrong"); // TODO add scenarios that hit this
         }
     }
 }

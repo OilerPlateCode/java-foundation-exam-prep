5272194967fd775e8e47dbae3eba5d3bdf26fdf8
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-10-10 16:04:05 +0200

    learn - catching with a method signature

--- java-foundation-exam-prep/src/chadJavaIntroCourse/section8/FileInputExceptions.java
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section8/FileInputExceptions.java
@@ -1,15 +1,19 @@
 package chadJavaIntroCourse.section8;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class FileInputExceptions {
     public static void main(String[] args) {
         try {
     FileReader fileReader = new FileReader("file.txt");
-
+            oneWithMethodSignature();
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
 
+    public static void oneWithMethodSignature() throws FileNotFoundException {
+        FileReader fileReader = new FileReader("otherFile.txt");
     }
 }

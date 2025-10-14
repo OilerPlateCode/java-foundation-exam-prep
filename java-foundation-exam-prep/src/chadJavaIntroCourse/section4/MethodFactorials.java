3803b7a678df77e92df8fe638d41105fafc8770d
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-10-14 17:40:01 +0200

    learn - revising factorals using the iterative method

--- java-foundation-exam-prep/src/chadJavaIntroCourse/section4/MethodFactorials.java
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section4/MethodFactorials.java
@@ -4,12 +4,22 @@ public class MethodFactorials {
     public static void main(String[] args) {
         int num = 5;
         System.out.println("The factorial for " + num + " is " + factorial(5));
-
+        System.out.println("The factorial for " + num + " is " + longFactorial(5));
     }
     public static int factorial(int num) {
         if (num == 0) return 1;
         else {
             return num * factorial(num - 1);
         }
+    }
+
+    public static long longFactorial(long num) {
+        long factorial = 1;
+
+        for (long i = num; i > 0; i-- ) {
+           factorial *= i;
+        }
+
+        return factorial;
     }
 }

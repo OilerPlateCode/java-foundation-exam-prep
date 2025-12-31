382c0e6999994bd96e5a6e4da28920bc2f57dfd0
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-12-31 20:00:10 +0200

    learn - learning initializing arrays

---
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section5/PlannedTripsDemo.java
@@ -0,0 +1,24 @@
+package chadJavaIntroCourse.section5;
+
+import java.util.Scanner;
+
+public class PlannedTripsDemo {
+    public static void main(String[] args) {
+        int numberOfTrips;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of trips: ");
+        numberOfTrips = sc.nextInt();
+        String[] tripTypes = new String[numberOfTrips];
+        String tripType;
+
+        for (int i = 0; i < numberOfTrips; i++) {
+            System.out.print("Enter trip type: ");
+            tripType = sc.next();
+            tripTypes[i] = tripType;
+        }
+
+        for (String trip : tripTypes) {
+            System.out.println("I want to go " + trip);
+        }
+    }
+}

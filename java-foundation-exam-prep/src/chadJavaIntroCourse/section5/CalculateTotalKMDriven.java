2a28d53c7aa21750157db3853f83c765c2e3927a
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2026-01-01 09:53:08 +0200

    learn - computing total from arrays

---
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section5/CalculateTotalKMDriven.java
@@ -0,0 +1,28 @@
+package chadJavaIntroCourse.section5;
+
+import java.util.Scanner;
+
+public class CalculateTotalKMDriven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of trips: ");
+        int numberOfTrips = scanner.nextInt();
+        float[] kmDriven = new float[numberOfTrips];
+
+        for (int i = 0; i < numberOfTrips; i++) {
+            System.out.println("Enter a KM driven amount: ");
+            kmDriven[i] = scanner.nextFloat();
+        }
+
+        System.out.println("We drove a total of: " + calculateTotal(kmDriven));
+    }
+
+    private static float calculateTotal(float[] numberOfTrips) {
+        float totalAmout = 0;
+        for (float distance: numberOfTrips) {
+            totalAmout += distance;
+        }
+        return totalAmout;
+    }
+
+}

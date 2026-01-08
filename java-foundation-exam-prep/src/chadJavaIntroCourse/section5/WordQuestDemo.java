f2c06a30b1d5d4fc6ed309019eeae7b083da069e
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2026-01-08 20:53:13 +0200

    learn - creating the blank space for the word to guess

--- java-foundation-exam-prep/src/chadJavaIntroCourse/section5/WordQuestDemo.java
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section5/WordQuestDemo.java
@@ -1,10 +1,16 @@
 package chadJavaIntroCourse.section5;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class WordQuestDemo {
     public static void main(String[] args) {
         String secretWord = getRandomWord();
+
+        int maxAttempts = 10;
+        char[] gameBoard = new char[secretWord.length()];
+
+        Arrays.fill(gameBoard, '_');
     }
 
     private static String getRandomWord() {

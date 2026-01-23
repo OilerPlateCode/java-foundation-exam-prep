0918cc82a800037a76e0032cbbd58600a769b1ce
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2026-01-24 00:44:18 +0200

    correcting typo

--- java-foundation-exam-prep/src/chadJavaIntroCourse/section5/WordQuestDemoV2.java
+++ java-foundation-exam-prep/src/chadJavaIntroCourse/section5/WordQuestDemoV2.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class WordQuestDemoV2 {
     public static void main(String[] args) throws IOException {
 
-        String fileName = "data/sample.txt";
+        String fileName = "data/sample-words.txt";
         String secretWord = getRandomWord(fileName);
 
         System.out.println("TESTING: " + secretWord);
